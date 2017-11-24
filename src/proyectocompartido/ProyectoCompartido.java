20223a0ecd832e14ccab5e706a1288b6290b1cc6
Johanna Villacís <33941995+JohannaVillacis1@users.noreply.github.com>
2017-11-24 13:17:27 -0500

    Update ProyectoCompartido.java

--- src/proyectocompartido/ProyectoCompartido.java
+++ src/proyectocompartido/ProyectoCompartido.java
@@ -21,6 +21,10 @@ public class ProyectoCompartido {
         System.out.println("2");
         System.out.println("3");
         System.out.println("4");
+        System.out.println("5");
+        System.out.println("6");
+        System.out.println("7");
+        
 
 
     }
